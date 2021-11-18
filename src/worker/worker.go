b8f99a6b1f6d6bbe03e4941d33fa05a180241c7d
albertlockett <albert.lockett@sonraisecurity.com>
2021-11-17 22:10:31 -0500

    it calls graphql-server

--- src/worker/worker.go
+++ src/worker/worker.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/golang/protobuf/proto"
 	gen "github.com/sonraisecurity/sonrai-asearch/src/proto"
 	"github.com/sonraisecurity/sonrai-asearch/src/queue"
+	"io/ioutil"
 	"log"
+	"net/http"
 )
+
+type GraphFilterMessage struct {
+	Filters []*gen.Filter
+}
 
 type Worker struct {
 	queue *queue.Queue
@@ -61,5 +69,38 @@ func (w*Worker) Deserialize(data []byte) (*gen.InProgressRecord, error) {
 
 func (w*Worker) Handle(record *gen.InProgressRecord) error {
 	log.Printf("Handling message %v", record)
+
+	// TODO
+	// - find the actual step
+	// - have different logic for processing steps
+	// - take the sources and somehow add to query
+
+	message := GraphFilterMessage{
+		Filters: record.Search.Steps[0].Filters,
+	}
+
+	data, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.Post("http://localhost:8080/records", "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	ids := make([]int64, 0)
+	err = json.Unmarshal(body, &ids)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("%v", ids)
 	return nil
 }
